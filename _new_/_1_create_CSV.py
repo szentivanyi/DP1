5c9de73c61f84c8751c90a3ce6bd9312c3ecc0f2
Ghost <denisszentivanyi@gmail.com>
2017-12-01 00:23:17 +0100

    * refactor

--- _new_/_1_create_CSV.py
+++ _new_/_1_create_CSV.py
@@ -4,12 +4,11 @@ COUNT_DICTIONARY_DOCS = 500  # Number of documents to create dictionary
 
 import csv
 from nltk.corpus import reuters
-from create_train_data import create_train_data  # Import train data
-from create_test_data import create_test_data  # Import test data
 from create_dictionary import create_dictionary
+from _create_in_out_vectors import create_in_out_vectors
 
 
-# rozdelim si db dokumentov do trenovacej a testovacej mnoziny (list-u)
+# rozdelim si dokumenty do trenovacej a testovacej mnoziny (list-u), beriem do uvahy len tie s 1 topikom
 categories = reuters.categories()
 train_docs = []
 test_docs = []
@@ -21,6 +20,7 @@ for doc_id in reuters.fileids():
         else:
             test_docs.append(doc_id)
 
+
 print("Topikov/kategorii: " + str(len(categories)))
 print("Training docs: " + str(len(train_docs)))
 print("Testing docs: " + str(len(test_docs)))
@@ -29,24 +29,29 @@ print("Testing docs: " + str(len(test_docs)))
 # vytvorim slovnik
 dictionary = create_dictionary(train_docs[:COUNT_DICTIONARY_DOCS], keep_percent=80)
 
+
 # vytvorim train a test data vektory
-train_data = create_train_data(train_docs[:COUNT_PROCESS_DOCS], dictionary)
-test_data = create_test_data(train_docs[:COUNT_PROCESS_DOCS], dictionary)
+train_data = create_in_out_vectors(train_docs[:COUNT_PROCESS_DOCS], dictionary)
+test_data = create_in_out_vectors(test_docs[:COUNT_PROCESS_DOCS], dictionary)
 # print(train_data)
 # print(test_data)
-# print("----------")
 
 
 # vytvorim train_data.CSV
 with open('train_data.csv', 'w') as f:
     writer = csv.writer(f)
-    writer.writerow([len(train_data), len(train_data[0])-1, categories])  # HLAVICKA CSV: pocet riadkov/dokumentov, pocet vlastnosti = dlzka 1 FV, vypisane vsetky kategorie
-    writer.writerows(train_data)    # Feature data + posledny stlpec v riadku je Label
+    writer.writerow([len(train_data), len(train_data[0])-1, categories])  # HLAVICKA CSV:
+                                                                        # pocet riadkov( 1 riadok = 1 in/out vector = 1 dokument),
+                                                                      # pocet vlastnosti = dlzka 1 FV (-1 co je LABEL),
+                                                                    # vypisane vsetky kategorie
+    writer.writerows(train_data)    # v riadkoch su Feature data + posledny stlpec v riadku je label
+
 
 # vytvorim test_data.CSV
 with open('test_data.csv', 'w') as f:
     writer = csv.writer(f)
-    writer.writerow([len(test_data), len(test_data[0])-1, categories]) # HLAVICKA CSV
+    writer.writerow([len(test_data), len(test_data[0])-1, len(categories), categories])  # HLAVICKA CSV
     writer.writerows(test_data)  # Feature data + posledny stlpec v riadku je Label
+
 
 print("OK, documents was created!")
--- _new_/_create_in_out_vectors.py
+++ _new_/_create_in_out_vectors.py
@@ -1,9 +1,12 @@
+# vytvareme vektory, kde posledny prvok hovori o tom, do akej kategorie text patri, zvysne prvky urcuju vlastnosti dokumentu
 from create_dictionary import process_doc, remove_stop_words
 from nltk.corpus import reuters
 
 
-def create_test_data(docs, dictionary):
+def create_in_out_vectors(docs, dictionary):
     all_topics = reuters.categories()
+
+    #### IN/FV ####
     test_data = []
     for i in range(len(docs)):
         # Process 1 raw document to list without numbers, signs, etc.
@@ -17,15 +20,14 @@ def create_test_data(docs, dictionary):
         # number of stop words in document
         sw_count = len_wordlist - len_wordlist_no_sw
 
-        # Replace words from doc not included in our dictionary by __OOV__
+        # Replace words from document not included in our dictionary by 0 or __OOV__
         fv = []
         fv_nn = []
-        for word in dictionary:   # v tvare list slovnikov [ {2:x} , {5:y} ...]
-            if word[-1] in wordlist:  # x, y
+        for word in dictionary:   # v tvare list slovnikov [{2 : slovoX}, {5 : slovoY}...]
+            if word[-1] in wordlist:  # slovoX
                 count = wordlist.count(word[-1])  # pocet vyskytov daneho slova v dokumente
                 # fv.append(dict({count:word[-1]}))  # human readable FV
-                # fv_nn.append(count)     # NN readable FV
-                fv_nn.append(((count/len_wordlist)*100))     # vyskyt daneho slova v dokumente v %
+                fv_nn.append(((count/len_wordlist)*100))   # vyskyt daneho slova v povodnom dokumente v % #TODO v povodnom alebo uz bez stop slov
             else:
                 # fv.append('__OOV__')
                 fv_nn.append(0.)
@@ -39,7 +41,8 @@ def create_test_data(docs, dictionary):
         # fv_nn.append(float("{0:.2f}".format((sw_count/len_wordlist)*100)))
         fv_nn.append((sw_count/len_wordlist)*100)
 
-        # na akej pozicii sa nachadza topic v zozname vsetkych topicov, jeho pozicia je UNIQUE, pripojime na koniec FV
+        #### OUT/LV ####
+        # na koniec FV pripojime LABEL=TOPIC dokumentu (na akej pozicii sa nachadza topic v zozname vsetkych topicov, je UNIQUE)
         doc_topic = reuters.categories(docs[i])
         topic_id = all_topics.index(doc_topic[0])
         fv_nn.append(topic_id)
@@ -47,9 +50,4 @@ def create_test_data(docs, dictionary):
         # V kazdom cykle spracujeme 1 dokument a prilepime jeho vektor vlastnosti + topic id k testovacim datam
         test_data.append(fv_nn)
 
-    return test_data  # all Feature Vectors as list + result topic
-
-
-
-
-
+    return test_data  # all Feature Vectors + Labels as list
